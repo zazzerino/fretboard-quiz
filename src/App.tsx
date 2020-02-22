4b4dd3703ab0fb24cc55a6cb600fa23e39748129
Kyle Patterson <patterkyle@gmail.com>
2020-02-22 14:30:02 -0600

    resets round on keypress

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Stave } from './components/Stave';
 import { Fretboard } from './components/Fretboard';
@@ -6,17 +6,30 @@ import { NewNoteButton } from './components/NewNoteButton';
 import { UserScore } from './components/UserScore';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppState, Status } from './types';
-
-// function handleKeyPress(dispatch: any, event: KeyboardEvent) {
-//   switch (event.key) {
-//     case 'Enter':
-//       dispatch({ type: })
-//   }
-// }
+import { newNoteToGuess } from './actions';
 
 export default function App() {
   const dispatch = useDispatch();
   const status = useSelector((state: AppState) => state.status);
+
+  function handleKeyPress(event: KeyboardEvent) {
+    if (status === Status.PLAYING) { return; };
+
+    switch (event.key) {
+      case 'Enter':
+        dispatch(newNoteToGuess());
+      case ' ':
+        dispatch(newNoteToGuess());
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keypress', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keypress', handleKeyPress);
+    }
+  });
 
   return (
     <div className="App">
@@ -27,25 +40,3 @@ export default function App() {
     </div>
   )
 }
-
-// function PlayingView() {
-//   return (
-//     <div className="PlayingView">
-//       <UserScore />
-//       <Stave />
-//       <Fretboard />
-//       <NewNoteButton />
-//     </div>
-//   );
-// }
-
-  // return (
-  //   <div className="App">
-  //     {(() => {
-  //       switch (status) {
-  //         default:
-  //           return <PlayingView />;
-  //       }
-  //     })()}
-  //   </div>
-  // );
