f2fc6a5f12ec3e74df2e6098b7d27faacebd3986
Kyle Patterson <Kyle Patterson>
2020-07-15 18:23:57 -0500

    init

---
+++ app/scores.py
@@ -0,0 +1,27 @@
+import collections
+from flask import (
+    Blueprint
+)
+
+from app.db import get_db
+
+bp = Blueprint('scores', __name__, url_prefix='/scores')
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+@bp.route('/', methods=['GET'])
+def view_scores():
+    db = get_db()
+    db.row_factory = dict_factory
+    c = db.cursor()
+
+    c.execute('SELECT * FROM scores')
+    scores = c.fetchall()
+
+    return {'scores': scores}
