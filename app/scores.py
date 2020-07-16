149bb5410e119259d05bfb9e2696c6cb23bf0b78
Kyle Patterson <Kyle Patterson>
2020-07-16 18:29:45 -0500

    creates score by POST

--- app/scores.py
+++ app/scores.py
@@ -1,6 +1,6 @@
 import collections
 from flask import (
-    Blueprint
+    Blueprint, request, make_response
 )
 
 from app.db import get_db
@@ -21,8 +21,27 @@ def view_scores():
     db.row_factory = dict_factory
 
     c = db.cursor()
-    c.execute('SELECT * FROM scores')
+    c.execute('SELECT * FROM scores ORDER BY score DESC')
 
     scores = c.fetchall()
 
     return {'scores': scores}
+
+
+@bp.route('/create', methods=['POST'])
+def create_score():
+    content = request.json
+    name = content['name']
+    score = content['score']
+
+    try:
+        db = get_db()
+        c = db.cursor()
+        sql = '''INSERT INTO scores (name, score) VALUES (?, ?)'''
+        c.execute(sql, (name, score))
+        db.commit()
+        return make_response('CREATED', 201)
+
+    except Exception:
+      return make_response('WHOOPS', 500)
+
