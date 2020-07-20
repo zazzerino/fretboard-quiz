b95158e09aebab49c102c3041f64030b0161b145
Kyle Patterson <patterkyle@gmail.com>
2020-07-19 20:46:15 -0500

    install.py copies files to /var/www

--- install.py
+++ install.py
@@ -3,22 +3,30 @@ import subprocess
 
 url = 'csasguitar.com'
 root_dir = '/home/kdp/projects/fretboard-quiz/'
+db_name = 'app.sqlite'
+service_name = 'fretboard-quiz.service'
 systemd_unit_dir = '/etc/systemd/system/'
 nginx_dir = '/etc/nginx/'
+owner_and_group = 'kdp:www-data'
+build_dir = os.path.join(root_dir, 'front', 'build')
+serve_dir = '/var/www'
 
 
 def setup_backend():
     backend_dir = os.path.join(root_dir, 'back')
     os.chdir(backend_dir)
+    print(f'setting up backend at {backend_dir}')
 
+    print('creating virtualenv')
     subprocess.run(['python3', '-m', 'venv', 'env'])
 
     venv_pip = os.path.join(backend_dir, 'env', 'bin', 'pip')
     venv_flask = os.path.join(backend_dir, 'env', 'bin', 'flask')
 
+    print('installing requirements')
     subprocess.run([venv_pip, 'install', '-r', 'requirements.txt'])
 
-    db_path = os.path.join(backend_dir, 'instance', 'app.sqlite')
+    db_path = os.path.join(backend_dir, 'instance', db_name)
     if not os.path.exists(db_path):
         print('creating db')
         subprocess.run([venv_flask, 'init-db'])
@@ -30,47 +38,88 @@ def setup_frontend():
     frontend_dir = os.path.join(root_dir, 'front')
     os.chdir(frontend_dir)
 
+    print(f'installing npm deps and building frontend at {build_dir}')
+
     subprocess.run(['npm', 'install'])
     subprocess.run(['npm', 'run', 'build'])
 
     print('frontend complete')
 
 
+def move_files():
+    os.chdir(serve_dir)
+    site_path = os.path.join(serve_dir, url)
+
+    print(f'removing {site_path}')
+    subprocess.run(['rm', '-rf', url])
+    subprocess.run(['mkdir', url])
+
+    print(f'copying index.html to {serve_dir}')
+    subprocess.run(['cp', '-r', os.path.join(root_dir, 'index.html'), site_path])
+
+    print(f'copying frontend to {site_path}/fretboard-quiz')
+    subprocess.run(['cp', '-r', build_dir, os.path.join(site_path, 'fretboard-quiz')])
+
+    print('setting owner and permissions')
+    subprocess.run(['chown', '-R', owner_and_group, site_path])
+    subprocess.run(['chmod', '-R', '755', site_path])
+    
+    print('files moved')
+
+
+def service_is_enabled(service_name):
+    res = subprocess.run(['systemctl', 'is-enabled', service_name], capture_output=True)
+    return res.stdout == b'linked\n'
+
+
 def setup_systemd_service():
     os.chdir(systemd_unit_dir)
+    service_path = os.path.join(root_dir, service_name)
+    print(f'starting {service_name} at {service_path}')
 
-    filename = 'fretboard-quiz.service'
-    filepath = os.path.join(root_dir, filename)
+    if os.path.exists(os.path.join(systemd_unit_dir, service_name)):
+        print('removing current service file')
+        subprocess.run(['rm', os.path.join(systemd_unit_dir, service_name)])
 
-    subprocess.run(['rm', os.path.join(systemd_unit_dir, filename)])
-    subprocess.run(['ln', '-s', filepath, filename])
-    subprocess.run(['systemctl', 'enable', filename])
-    subprocess.run(['systemctl', 'start', filename])
+    print(f'linking {service_name} at {systemd_unit_dir}')
+    subprocess.run(['ln', '-s', service_path, service_name])
 
-    print(f'{filename} started')
+    if service_is_enabled(service_name):
+        subprocess.run(['systemctl', 'restart', service_name])
+        print(f'{service_name} restarted')
+    else:
+        subprocess.run(['systemctl', 'enable', service_name])
+        subprocess.run(['systemctl', 'start', service_name])
+        print(f'{service_name} enabled and started')
 
 
 def setup_nginx():
     sites_available_path = os.path.join(nginx_dir, 'sites-available')
     sites_enabled_path = os.path.join(nginx_dir, 'sites-enabled')
 
+    print(f'linking {url} at {sites_available_path}')
     os.chdir(sites_available_path)
     subprocess.run(['rm', url])
     subprocess.run(['ln', '-s', os.path.join(root_dir, url)])
 
+    print(f'linking {url} at {sites_enabled_path}')
     os.chdir(sites_enabled_path)
     subprocess.run(['rm', url])
     subprocess.run(['ln', '-s', os.path.join(sites_available_path, url)])
 
-    subprocess.run(['systemctl', 'enable', 'nginx'])
-    subprocess.run(['systemctl', 'start', 'nginx'])
-
-    print('nginx started')
+    if service_is_enabled('nginx'):
+        subprocess.run(['systemctl', 'restart', 'nginx'])
+        print('nginx restarted')
+    else:
+        subprocess.run(['systemctl', 'enable', 'nginx'])
+        subprocess.run(['systemctl', 'start', 'nginx'])
+        print('nginx enabled and started')
 
 
 def main():
     setup_backend()
     setup_frontend()
+    move_files()
     setup_systemd_service()
     setup_nginx()
 
