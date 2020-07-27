a4ab7989d7cda5b044a6df5bd9c8dd005b2e2caa
Kyle Patterson <patterkyle@gmail.com>
2020-07-27 14:37:07 -0500

    test user apis

--- back/app/user.py
+++ back/app/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, make_response, request, jsonify
+from flask import Blueprint, request, jsonify
 from app import db
 from app.models import User
 
@@ -8,18 +8,25 @@ user_bp = Blueprint('user', __name__, url_prefix='/api/user')
 
 @user_bp.route('/create', methods=['POST'])
 def create():
-    name = request.json.get('name')
-    password = request.json.get('password')
-    email = request.json.get('email')
-    if name is None or password is None:
-        make_response('must provide username and password', 400)
-    if User.query.filter_by(name=name).first() is not None:
-        make_response('username already taken', 400)
-    user = User(name=name, email=email)
-    user.set_password(password)
-    db.session.add(user)
-    db.session.commit()
-    return make_response({'user': user.name}, 201)
+    if request.is_json:
+        name = request.json.get('name', None)
+        password = request.json.get('password', None)
+        email = request.json.get('email', None)
+
+        if name is None or password is None:
+            return 'must provide username and password', 400
+
+        if User.query.filter_by(name=name).first() is not None:
+            return 'username already taken', 400
+
+        user = User(name=name, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return {'user': {'name': user.name,
+                         'email': user.email}}, 201
+    else:
+        return 'must provide valid json', 400
 
 
 @user_bp.route('/<name>')
--- back/tests/app_test.py
+++ back/tests/app_test.py
@@ -22,18 +22,38 @@ def client():
     os.unlink(test_db_pathname)
 
 
-def test_root(client):
+def test_index(client):
     response = client.get('/')
-    assert response is not None
     assert response.status_code == 404
 
 
-def test_create_user(client):
+def test_user(client):
+    response = client.post('/api/user/create')
+    assert response.status_code == 400
+    assert response.data == b'must provide valid json'
+
     response = client.post('/api/user/create',
                            content_type='application/json',
                            data=json.dumps(dict(
                                name='bob',
                                password='hunter2'
                            )))
-
+    data = response.get_json()
     assert response.status_code == 201
+    assert data['user']['name'] == 'bob'
+    assert data['user']['email'] is None
+
+    response = client.post('/api/user/create',
+                           content_type='application/json',
+                           data=json.dumps(dict(
+                               name='bob',
+                               password='hunter2'
+                           )))
+    assert response.status_code == 400
+    assert response.data == b'username already taken'
+
+    response = client.get('/api/user/bob')
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['user']['name'] == 'bob'
+    assert data['user']['email'] is None
